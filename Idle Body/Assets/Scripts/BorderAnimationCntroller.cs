f1aa31d216d6042ce023dcc1e7c190781be67f1c
Juan Felipe Medrano <35904953+Acidoswaldo@users.noreply.github.com>
2021-03-01 12:09:37 -0500

    revisando animaciones de bordes (corazón)

---
+++ Idle Body/Assets/Scripts/BorderAnimationCntroller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BorderAnimationCntroller : MonoBehaviour
+{
+    public void animationLeftAcrive()
+    {
+        GetComponent<Animator>().SetTrigger("IZ");
+    }
+}
