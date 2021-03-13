3de1fd5c4fecb1b44da2885b0663b523bab90cf5
Rafael Ospina <rafaelospiname@gmail.com>
2021-03-12 22:01:37 -0500

    Hice que el número de puntos por segundo se determine por la cantidad de células que haya en la escena

--- Idle Body/Assets/Scripts/RafaTestingScripts/PointsManager.cs
+++ Idle Body/Assets/Scripts/RafaTestingScripts/PointsManager.cs
@@ -13,18 +13,20 @@ public class PointsManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(GetPointsPerSecond());
+        StartCoroutine(GetPointsPerSecond()); //Empieza la Coroutine que cada segundo agrega los puntos
     }
 
     // Update is called once per frame
     void Update()
     {
+        //El valor de los puntos por segundo se consigue con el número de células que haya en la escena. Se podría
+        //guardar un valor aparte por órgano en GameStats y que se sumen los valores de cada órgano para el total
+        pointsPerSecond = GameObject.FindGameObjectsWithTag("RedCells1").Length + (GameObject.FindGameObjectsWithTag("RedCells10").Length * 10) + (GameObject.FindGameObjectsWithTag("RedCells100").Length * 100);
+        
         total.text = "Total points: " + totalPoints.ToString();
         points.text = "Points per second: " + pointsPerSecond.ToString();
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            pointsPerSecond *= 2;
-        }
+
+        
         if (Input.GetKeyDown(KeyCode.O))
         {
             GetPoints(4);
@@ -44,5 +46,6 @@ public class PointsManager : MonoBehaviour
     public void GetPoints(float pointValue)
     {
         totalPoints += pointValue;
+        // Esta función es para agregar puntos manualmente. Por ejemplo haciendo tap en la pantalla
     }
 }
