44c3312883d0eb9907d784597ebbfb97ead1c7ad
Rafael Ospina <rafaelospiname@gmail.com>
2021-10-04 11:04:04 -0500

    Push

--- Idle Body/Assets/Scripts/GamePlay/Pathogens/Pathogen_Virus.cs
+++ Idle Body/Assets/Scripts/GamePlay/Pathogens/Pathogen_Virus.cs
@@ -8,21 +8,27 @@ public class Pathogen_Virus : Pathogen_Base
     {
         Move();
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.transform == target && move)
         {
+            print("collided");
             if (target.GetComponent<HitPoints>().canDie)
             {
                 target.GetComponent<HitPoints>().health -= 1;
+                if (target.GetComponent<HitPoints>().health <= 0)
+                {
+                    OnPathogenEffect();
+                }
                 //GetComponent<HitPoints>().hitPoints -= 1;
                 StartCoroutine(MoveAgain());
             }
         }
     }
+
     protected override void OnPathogenEffect()
     {
-        pathogenSpawner.AddPathogen(2, myOrganManager.activeOrganType, myOrganManager.activeOrganID);
+        pathogenSpawner.AddPathogen(1, myOrganManager.activeOrganType, myOrganManager.activeOrganID);
         pathogenSpawner.SpawnPathogens(false, transform);
     }
 }
